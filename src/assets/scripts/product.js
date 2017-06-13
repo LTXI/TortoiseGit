c2703669c622b28e94febd2d8adfaf4a069bb765
LTXI <ltxlife@163.com>
2017-06-13 09:41:37 +0800

    add

---
+++ src/assets/scripts/product.js
@@ -0,0 +1,18 @@
+$(function () {
+  $(".icon-popups").on('click', function (e) {
+
+    //$("body").append('<div class="cover-normal"></div>');
+    //$("#s-activity-wrapper").css('display', 'block');
+    //setTimeout(function () {
+     // $("#s-activity-wrapper").addClass("show");
+    //}, 10);
+
+  });
+  $("body").on('click', '.cover-normal', function () {
+    //$("#s-activity-wrapper").removeClass("show");
+    //setTimeout(function () {
+     // $("#s-activity-wrapper").css('display', 'none');
+      //$(".cover-normal").remove();
+    //}, 200);
+  })
+});
